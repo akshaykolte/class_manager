50dae5895eca06665ad13854aaa202261e376e52
AkshayAsawa <akshayasawa94@gmail.com>
2015-07-11 19:01:56 +0530

    Functions added

---
+++ portal/db_api/academic_year_db.py
@@ -0,0 +1,4 @@
+# add/modify academic year
+# get academic year
+# set current academic year
+# get current academic year
---
+++ portal/db_api/fee_db.py
@@ -0,0 +1,2 @@
+# add transaction
+# get transaction (one or all of one student or of any particular type)
