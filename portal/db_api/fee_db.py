4faee5067b2ac6509afadd681638f693078afa03
AkshayAsawa <akshayasawa94@gmail.com>
2015-07-13 02:17:43 +0530

    Fees functions added

--- portal/db_api/fee_db.py
+++ portal/db_api/fee_db.py
@@ -1,2 +1,4 @@
-# add transaction
-# get transaction (one or all of one student or of any particular type)
+# set_base_fee
+# get_base_fee
+# add_transaction
+# get_transaction (one or all of one student or of any particular type)
