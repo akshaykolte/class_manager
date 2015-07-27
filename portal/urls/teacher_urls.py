48e3a2ebaf47bef4acd9b7a177a7c01c51561d83
AkshayAsawa <akshayasawa94@gmail.com>
2015-07-28 02:52:41 +0530

    Base teacher made in new UI

--- portal/urls/teacher_urls.py
+++ portal/urls/teacher_urls.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^dashboard/$', 'portal.views.teacher_views.dashboard'),
+    url(r'^attendance/mark-attendance/$', 'portal.views.teacher_views.mark_attendance'),
+    url(r'^attendance/view-attendance/$', 'portal.views.teacher_views.view_attendance'),
 ]
