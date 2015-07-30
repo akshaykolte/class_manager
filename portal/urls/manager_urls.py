e9cd2b4c41fc3347b2bfa15d230482fb8f43a3fd
asdoc <asdocode@gmail.com>
2015-07-31 03:05:19 +0530

    Partial implementation of add lecture

--- portal/urls/manager_urls.py
+++ portal/urls/manager_urls.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^dashboard/$', 'portal.views.manager_views.dashboard'),
+    url(r'^lectures/add-lectures/$', 'portal.views.manager_views.add_lectures'),
 ]
