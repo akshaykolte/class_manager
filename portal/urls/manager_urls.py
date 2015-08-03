c1fcc8e01c9491e836ed9c8d894043e5c9c22ee0
asdoc <asdocode@gmail.com>
2015-08-03 21:08:03 +0530

    View/Edit lectures added

--- portal/urls/manager_urls.py
+++ portal/urls/manager_urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^dashboard/$', 'portal.views.manager_views.dashboard'),
     url(r'^lectures/add-lectures/$', 'portal.views.manager_views.add_lectures'),
+	url(r'^lectures/view-lectures/$', 'portal.views.manager_views.view_lectures'),
 ]
--- portal/views/manager_views.py
+++ portal/views/manager_views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render,redirect
 from django.views.decorators.csrf import csrf_exempt
 from portal.db_api.standard_db import *
 from portal.db_api.subject_db import *
+from portal.db_api.auth_db import *
 from portal.db_api.lecture_db import *
+from django.http import Http404
 
 def dashboard(request):
+	auth_dict = get_user(request)
+	
+	if auth_dict['logged_in'] != True:
+		raise Http404
+	
+	if auth_dict['permission_manager'] != True:
+		raise Http404
+
 	return render(request,'manager/dashboard.html')
 
 @csrf_exempt
@@ -18,6 +28,14 @@ def add_lectures(request):
 		Post request for adding the lecture: /manager/lectures/add_lectures/
 	'''
 	
+	auth_dict = get_user(request)
+	
+	if auth_dict['logged_in'] != True:
+		raise Http404
+	
+	if auth_dict['permission_manager'] != True:
+		raise Http404
+	
 	if request.method == 'GET':
 		context = {}
 		if 'message' in request.GET:
@@ -26,10 +44,6 @@ def add_lectures(request):
 			context['message_error'] = request.GET['message_error']
 		
 		page_type = 0
-		if 'standard' in request.GET:
-			page_type += 1
-		if 'subject' in request.GET:
-			page_type += 2
 		
 		standards = get_standard()
 		context['standards'] = standards
@@ -57,3 +71,68 @@ def add_lectures(request):
 			return redirect('./?message=Lecture Added')
 		except:
 			return redirect('./?message_error=Error Adding Lecture')
+
+@csrf_exempt
+def view_lectures(request):
+	'''
+		Page Type					GET  (optional: 'message')
+				0					None
+				1					standard
+				2					standard, subject
+				3					lecture edit
+		Post request for editing the lecture: /manager/lectures/view_lectures/
+	'''
+
+	auth_dict = get_user(request)
+	if auth_dict['logged_in'] != True:
+		raise Http404
+	
+	if auth_dict['permission_manager'] != True:
+		raise Http404
+
+	
+	if request.method == 'GET':
+		context = {}
+		if 'message' in request.GET:
+			context['message'] = request.GET['message']
+		elif 'message_error' in request.GET:
+			context['message_error'] = request.GET['message_error']
+		
+		page_type = 0
+		if 'standard' in request.GET:
+			page_type += 1
+		if 'subject' in request.GET:
+			page_type += 2
+		
+		standards = get_standard()
+		context['standards'] = standards
+		
+		if 'standard' in request.GET:
+			page_type = 1
+			subjects = get_subjects(standard_id=request.GET['standard'])
+			context['subjects'] = subjects
+			context['standard_id'] = int(request.GET['standard'])
+			if 'subject' in request.GET:
+				page_type = 2
+				context['subject_id'] = int(request.GET['subject'])
+				context['lectures'] = get_lecture(subject_year_id=context['subject_id'])
+		elif 'lecture' in request.GET:
+			page_type = 3
+			context['lecture'] = get_lecture(id=request.GET['lecture'])
+
+		context['page_type'] = page_type
+
+
+		return render(request,'manager/lectures/view_lectures.html', context)
+
+	elif request.method == 'POST':
+		try:
+			lecture_id = request.POST['lecture']
+			lecture_name = request.POST['lecture_name']
+			lecture_description = request.POST['lecture_description']
+			lecture_count = request.POST['lecture_count']
+
+			set_lecture(id=lecture_id, name=lecture_name, description=lecture_description, count=lecture_count)
+			return redirect('./?message=Lecture Edited')
+		except:
+			return redirect('./?message_error=Error Editing Lecture')
