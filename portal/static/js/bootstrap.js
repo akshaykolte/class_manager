51772112b506a3573ea132fb97a0064f63a7b57a
akshayapte <akshayapte7@gmail.com>
2015-09-02 13:04:29 +0530

    accountant datepicker added

--- portal/static/js/bootstrap.js
+++ portal/static/js/bootstrap.js
@@ -83,7 +83,7 @@ if (typeof jQuery === 'undefined') {
  * Copyright 2011-2015 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
-
+$('#datepicker').datepicker();
 
 +function ($) {
   'use strict';
