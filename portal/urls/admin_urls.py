c09528a70dfc29385c1c875a21ebe8bc440067a3
AkshayAsawa <akshayasawa94@gmail.com>
2015-09-02 16:45:43 +0530

    Graphical overview template added

--- portal/urls/admin_urls.py
+++ portal/urls/admin_urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^staff/view-staff/$', 'portal.views.admin_views.view_staff'),
     url(r'^staff/assign-roles/$', 'portal.views.admin_views.assign_roles'),
     url(r'^set-current-academic-year/$', 'portal.views.admin_views.set_current_academic_year_view'),
-    url(r'^track-progress/$', 'portal.views.admin_views.track_progress'),
+    url(r'^track-progress/detailed-progress/$', 'portal.views.admin_views.detailed_progress'),
+    url(r'^track-progress/graphical-overview/$', 'portal.views.admin_views.graphical_overview'),
 ]
