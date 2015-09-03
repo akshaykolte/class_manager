e2ae3e8ae2315181b21c2dba6c5facff4a4a6073
AkshayAsawa <akshayasawa94@gmail.com>
2015-09-03 17:55:55 +0530

    LOC Calculator pushed

---
+++ findLOC.sh
@@ -0,0 +1,34 @@
+echo "Python files: "
+find . -name "*.py" -type f -exec cat {} + | wc -l
+
+echo ""
+
+echo "HTML files: "
+find . -name "*.html" -type f -exec cat {} + | wc -l
+
+echo ""
+
+echo "CSS files: "
+find . -name "*.css" -type f -exec cat {} + | wc -l
+
+echo ""
+
+echo "JS files: "
+find . -name "*.js" -type f -exec cat {} + | wc -l
+
+echo ""
+
+echo "Text files: "
+find . -name "*.txt" -type f -exec cat {} + | wc -l
+
+echo ""
+
+echo "Markdown files: "
+find . -name "*.md" -type f -exec cat {} + | wc -l
+
+echo ""
+
+echo "===TOTAL==="
+find . -name "*" -type f -exec cat {} + | wc -l
+
+echo ""
