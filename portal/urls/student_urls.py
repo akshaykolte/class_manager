0499d99e34c1f40cf94a9c678fdd575186704126
akshaykolte <akki.kolte@gmail.com>
2015-10-08 16:31:11 +0530

    Added fee status in Student

--- portal/urls/student_urls.py
+++ portal/urls/student_urls.py
@@ -15,4 +15,7 @@ urlpatterns = [
     url(r'^dashboard/$', 'portal.views.student_views.dashboard'),
     url(r'^attendance/view-attendance/$', 'portal.views.student_views.view_attendance'),
     url(r'^lecture/view-lectures/$', 'portal.views.student_views.view_lectures'),
+    
+    #Fees
+    url(r'^fees/view-fees/$', 'portal.views.student_views.view_fees'),
 ]
--- portal/views/student_views.py
+++ portal/views/student_views.py
@@ -5,6 +5,7 @@ from portal.db_api.student_db import *
 from portal.db_api.auth_db import *
 from portal.db_api.attendance_db import *
 from portal.db_api.lecture_db import *
+from portal.db_api.fee_db import *
 from portal.db_api.attendance_reports_db import *
 from portal.db_api.notice_db import *
 import datetime
@@ -17,8 +18,7 @@ def dashboard(request):
 	if auth_dict['logged_in'] == False:
 		raise Http404
 
-	if auth_dict['permission_student'] != True:
-		raise Http404
+	
 	context['auth_dict'] = auth_dict
 	context['notices'] = get_personal_notices(student_id=auth_dict['id'], for_students =True)
 	return render(request,'student/dashboard.html', context)
@@ -30,9 +30,7 @@ def view_profile(request):
 	if auth_dict['logged_in'] == False:
 		raise Http404
 
-	if auth_dict['permission_student'] != True:
-		raise Http404
-
+	
 	details = get_students(id = auth_dict['id'])
 	context['auth_dict'] = auth_dict
 	context['details'] = details
@@ -47,9 +45,7 @@ def edit_profile(request):
 	if auth_dict['logged_in'] == False:
 		raise Http404
 
-	if auth_dict['permission_student'] != True:
-		raise Http404
-
+	
 	details = get_students(id=auth_dict['id'])
 
 	context['auth_dict'] =auth_dict
@@ -64,9 +60,7 @@ def view_attendance(request):
 	if auth_dict['logged_in'] == False:
 		raise Http404
 
-	if auth_dict['permission_student'] != True:
-		raise Http404
-
+	
 	page_type = 1
 	student_batch = get_student_batch(student_id=auth_dict['id'])
 	subjects = student_batch['student_subjects']
@@ -94,8 +88,6 @@ def view_lectures(request):
 	if auth_dict['logged_in'] == False:
 		raise Http404
 
-	if auth_dict['permission_student'] != True:
-		raise Http404
 	context['auth_dict'] = auth_dict
 	student_object = get_student_batch(student_id = auth_dict['id'])
 	lecture_list = []
@@ -128,9 +120,7 @@ def edit_profile_submit(request):
 	if auth_dict['logged_in'] == False:
 		raise Http404
 
-	if auth_dict['permission_student'] != True:
-		raise Http404
-
+	
 	set_student(id = auth_dict['id'], first_name = request.POST['first_name'], last_name = request.POST['last_name'], address = request.POST['address'], email = request.POST['email'], phone_number = request.POST['phone_number'], gender = request.POST['gender'])
 
 	return redirect('/student/profile/view-profile')
@@ -143,8 +133,6 @@ def change_password(request):
 	if auth_dict['logged_in'] == False:
 		raise Http404
 
-	if auth_dict['permission_student'] != True:
-		raise Http404
 	if 'message' in request.GET:
 		context['message'] = request.GET['message']
 
@@ -160,9 +148,45 @@ def change_password_submit(request):
 	if auth_dict['logged_in'] == False:
 		raise Http404
 
-	if auth_dict['permission_student'] != True:
-		raise Http404
 	if change_password_db(request):
 		return redirect('/student/profile/change-password/?message=Password Successfully Changed')
 	else:
 		return redirect('/student/profile/change-password/?message_error=Password Change Failed')
+		
+		
+		
+#Fees-------------------------------------------------------------
+
+def view_fees(request):
+	auth_dict = get_user(request)
+
+	if auth_dict['logged_in'] == False:
+		raise Http404
+
+	context = {}
+	context['details'] = auth_dict
+	if request.method == 'GET':
+		try:
+			page_type = 0
+			
+			fee_details = get_student_fees( student_id = auth_dict['id'] )
+			transaction_details = get_fee_transaction(id = None ,date_start = None, date_end = None, student_id = auth_dict['id'], fee_type_id = None)
+			#print fee_details
+			context['fee_details'] = fee_details
+			context['transaction_details'] = transaction_details
+
+			context['page_type'] = page_type
+			print context
+
+			return render(request,'student/fees/view-fees.html', context)
+		except ModelValidateError, e:
+			return redirect('./?message_error='+str(e))
+		except ValueError, e:
+			return redirect('./?message_error='+str(PentaError(1000)))
+		except ObjectDoesNotExist, e:
+			return redirect('./?message_error='+str(PentaError(999)))
+		except MultiValueDictKeyError, e:
+			return redirect('./?message_error='+str(PentaError(998)))
+		except Exception, e:
+			return redirect('./?message_error='+str(PentaError(100)))
+		
