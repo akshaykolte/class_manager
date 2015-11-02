a16cc8b0b2e62edaf6401e03b278344844279d78
asdoc <asdocode@gmail.com>
2015-11-02 01:31:59 -0500

    DB changes for demo

--- class_manager/settings.py
+++ class_manager/settings.py
@@ -82,11 +82,14 @@ WSGI_APPLICATION = 'class_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'Ja7RQKfHsZ',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
