3dd99c862b5b053c1993c3c112ff1f063783b1fb
AkshayAsawa <akshayasawa94@gmail.com>
2015-12-15 20:37:54 +0530

    SMS for attendance done (daemon left)

--- portal/db_api/sms_db.py
+++ portal/db_api/sms_db.py
@@ -1,4 +1,4 @@
-from portal.models import StudentBatch, SMS, StudentParent, Parent
+from portal.models import StudentBatch, SMS, StudentParent, Parent, Student
 SIGNATURE = "Your Class Name."
 
 def send_sms(phone_number, text):
@@ -7,8 +7,9 @@ def send_sms(phone_number, text):
 # Function that sends message to parents of absent students
 def sms_for_attendance(student_id_list, date):
 
-    for student_id in student_batch_id_list:
-        parent_id = StudentParent.objects.get(student__id=student_id)
+    for student_id in student_id_list:
+        student_object = Student.objects.get(id=student_id)
+        parent_id = StudentParent.objects.get(student=student_object).parent.id
         parent_phone_number = Parent.objects.get(id=parent_id).phone_number
         text = "Your ward " + student_object.first_name + " was absent on " + date + ". " + SIGNATURE
         sms_object = SMS(phone_number=parent_phone_number, sms_type = "Attendance", message_text=text, status="Pending", student=student_object)
