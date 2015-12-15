481f1b5ce92cfb0013072d849c6d5f9db7cf7969
AkshayAsawa <akshayasawa94@gmail.com>
2015-12-15 20:04:57 +0530

    sms_db modified student batch to student id

---
+++ portal/db_api/sms_db.py
@@ -0,0 +1,24 @@
+from portal.models import StudentBatch, SMS, StudentParent, Parent
+SIGNATURE = "Your Class Name."
+
+def send_sms(phone_number, text):
+    pass
+
+# Function that sends message to parents of absent students
+def sms_for_attendance(student_id_list, date):
+
+    date = str(date.day) + "/" + str(date.month) + "/" + str(date.year)
+
+    for student_id in student_batch_id_list:
+        parent_id = StudentParent.objects.get(student__id=student_id)
+        parent_phone_number = Parent.objects.get(id=parent_id).phone_number
+        text = "Your ward " + student_object.first_name + " was absent on " + date + ". " + SIGNATURE
+        sms_object = SMS(phone_number=parent_phone_number, sms_type = "Attendance", message_text=text, status="Pending", student=student_object)
+        sms_object.save()
+
+
+def sms_for_notices(student_batch_id_list):
+    pass
+
+def sms_for_marks(student_batch_id_list):
+    pass
