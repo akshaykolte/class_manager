50c93b45aadf33caf40fae0baada1c49c36d7aeb
xennygrimmato <vstulsyan@gmail.com>
2015-12-25 17:48:12 +0530

    BASE_DIR added to error_codes.py

--- portal/validator/error_codes.py
+++ portal/validator/error_codes.py
@@ -1,7 +1,9 @@
+from class_manager.settings import BASE_DIR
+
 class ErrorCode():
     def __init__(self):
         self.error_code_dict = {}
-        error_codes_list = open('portal/validator/error_codes.txt', 'r').read().split('\n')
+        error_codes_list = open(BASE_DIR + '/portal/validator/error_codes.txt', 'r').read().split('\n')
         try:
             for error_codes in error_codes_list:
                 self.error_code_dict[int(error_codes.split(' ', 1)[0])] = error_codes.split(' ',1)[1]
