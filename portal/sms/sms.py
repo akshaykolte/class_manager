654c70c729d510d994c92a19bd3c7b096bbff84e
Akshay Asawa <akshayasawa94@gmail.com>
2015-12-29 19:25:35 +0530

    trailing \n removed

--- portal/sms/sms.py
+++ portal/sms/sms.py
@@ -5,7 +5,7 @@ import requests
 def send_sms(message_text, phone_number):
 
     text = quote(message_text)
-    api_string = "http://bhashsms.com/api/sendmsg.php?user=9404792579&pass=8ac45b9&sender=MDHOBI&phone="+str(phone_number)+"&text="+text+"&priority=ndnd&stype=normal"
+    api_string = "http://bhashsms.com/api/sendmsg.php?user=9404792579&pass=8ac45b9&sender=MDHOBI&phone="+str(phone_number).strip()+"&text="+text+"&priority=ndnd&stype=normal"
     try:
     	return_string = requests.get(api_string).text
     	return True
